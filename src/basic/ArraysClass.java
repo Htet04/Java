53cfaf12aec083b0e0e35d4a1016d604493254cf
Khun Thein Zaw <aabb36335@gmail.com>
2022-09-22 21:05:53 +0630

    add ArraysClass

---
+++ src/basic/ArraysClass.java
@@ -0,0 +1,13 @@
+package basic;
+
+import java.util.Arrays;
+
+public class ArraysClass {
+    public static void main(String[] args){
+        int[] arr = new int[5];
+        arr[3] = 33;
+        System.out.println(arr[3]);
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
