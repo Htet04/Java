9433f3b806e1ce19f6047e7b2c6cfadb772f8e7e
Khun Thein Zaw <aabb36335@gmail.com>
2022-09-22 21:18:25 +0630

    add ArraysClass

--- src/basic/ArraysClass.java
+++ src/basic/ArraysClass.java
@@ -4,10 +4,15 @@ import java.util.Arrays;
 
 public class ArraysClass {
     public static void main(String[] args){
+        // Arrays
+        // Limit Arrays length
+        // Can't add more values
         int[] arr = new int[5];
-        arr[3] = 33;
-        System.out.println(arr[3]);
         System.out.println(Arrays.toString(arr));
 
+        // Un-Limited Arrays length
+        // can add more values
+        int[] arr1 = {2,9,2,3,3};
+        System.out.println(Arrays.toString(arr1));
     }
 }
