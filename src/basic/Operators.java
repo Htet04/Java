c426482fb31a41453170ef17588fe4b96ed6e9ff
Khun Thein Zaw <aabb36335@gmail.com>
2022-09-28 19:03:19 +0630

    update content

--- src/basic/Operators.java
+++ src/basic/Operators.java
@@ -1,4 +1,25 @@
 package basic;
 
 public class Operators {
+    public static void main(String[] args) {
+        /*
+        Precedence
+
+        Arithmetic operators
+        Assignment operators
+        Comparison operators
+        Logical operators
+        Bitwise operators
+
+        Arithmetic Operators are
+        + addition
+        - subtraction
+        * multiplication
+        / division
+        % modulus
+        ++ increment
+        -- decrement
+
+         */
+    }
 }
