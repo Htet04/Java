fe6dfb7414f4bc3d8892681d94b5a0bb49b89ddd
Khun Thein Zaw <aabb36335@gmail.com>
2022-09-28 18:26:29 +0630

    ch

--- src/basic/ArraysClass.java
+++ src/basic/ArraysClass.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class ArraysClass {
     public static void main(String[] args){
         // Arrays
+        // One Dimensional Arrays
         // Limit Arrays length
         // Can't add more values
         int[] arr = new int[5];
@@ -14,5 +15,14 @@ public class ArraysClass {
         // can add more values
         int[] arr1 = {2,9,2,3,3};
         System.out.println(Arrays.toString(arr1));
+
+        // Two Dimensional Arrays
+        int[][] matrix = {
+                {1,2,3},
+                {4,5,6}
+        };
+        System.out.println(matrix[0][2]);
+        System.out.println(Arrays.toString(matrix[0]));
+        System.out.println(Arrays.deepToString(matrix));
     }
 }
