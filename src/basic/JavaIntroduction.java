571d4d49270bcf5db85fab114966da39fc856295
Khun Thein Zaw <aabb36335@gmail.com>
2022-09-28 18:44:17 +0630

    re-arrange and update

---
+++ src/basic/JavaIntroduction.java
@@ -0,0 +1,31 @@
+package basic;
+
+public class JavaIntroduction {
+    public static void main(String[] args) {
+        /*
+        JRE --> Java Runtime Environment
+        JDK --> Java Development Kit
+        IDE --> Integrated Development Environment
+
+        DataType in java
+        Primitive & Non-Primitive
+        Primitive types are byte, short, int, int, long, double, char, boolean.
+        Non-Primitive types are Array, String,...(Object).
+
+        syntax
+        int num; initialization
+        num = 13; declaration
+        int num = 13; direct declaration
+
+        int --> DataType
+        num --> variable name
+        = --> assignment operator
+        ; --> end operator
+
+        java keywords
+        Every words that have mean to java language.(public,int,class,...)
+
+
+         */
+    }
+}
