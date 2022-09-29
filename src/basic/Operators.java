08b7526676b61311ebde05aba3a0939d4ea3b99c
Khun Thein Zaw <aabb36335@gmail.com>
2022-09-29 13:35:25 +0630

    update content

--- src/basic/Operators.java
+++ src/basic/Operators.java
@@ -20,6 +20,32 @@ public class Operators {
         ++ increment
         -- decrement
 
+        Assignment Operators
+        =
+        +=
+        -=
+        *=
+        /=
+        %=
+        &=
+        |=
+        ^=
+        <<=
+        >>=
+
+        Comparison Operators
+        == Equal To
+        != Not Equal
+        < Smaller Than
+        > Greater Than
+        <= Smaller Than and Equal
+        >= Greater Than and Equal
+
+        Logical Operators
+        && Logical And ( Return true if both statements are true )
+        || Logical Or ( Return true if one of the statement is true )
+        ! Logical Not ( Reverse the result, return true if the statement is false, return false if the statement is ture )
          */
+        System.out.println(0.1+0.2==0.3);
     }
 }
