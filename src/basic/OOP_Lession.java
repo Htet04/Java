d115a44b0deeab69d98956ba5f861fb7ff4e319b
Khun Thein Zaw <aabb36335@gmail.com>
2022-09-29 21:48:51 +0630

    update content

--- src/basic/OOP_Lession.java
+++ src/basic/OOP_Lession.java
@@ -13,5 +13,38 @@ public class OOP_Lession {
         Abstraction --> hide internal data, show function
         Encapsulation --> collect all data in one place
          */
+        TextBoxOne mytext = new TextBoxOne();
+        mytext.setText("This is My text.");
+        System.out.println(mytext.getText());
+        System.out.println(mytext.getTextLength());
+        mytext.clear();
+        System.out.println(mytext.getText());
+
+        var mysecondtext = new TextBoxOne();
+        mysecondtext.setText("This is My second text.");
+        System.out.println(mysecondtext.getText());
+        System.out.println(mysecondtext.getTextLength());
+        mysecondtext.clear();
+        System.out.println(mysecondtext.getText());
+    }
+    public static class TextBoxOne{
+        String text;
+        public TextBoxOne(){
+
+        }
+        public void setText(String text){
+            this.text = text;
+        }
+        public String getText(){
+            return text;
+        }
+
+        public void clear(){
+            this.text = null;
+        }
+
+        public int getTextLength(){
+            return text.length();
+        }
     }
 }
