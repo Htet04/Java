d2a8164650ff88d1b2b9c346e4a445eea1f61d9f
Khun Thein Zaw <aabb36335@gmail.com>
2022-09-29 16:11:14 +0630

    update content

---
+++ src/basic/OOP_Lession.java
@@ -0,0 +1,17 @@
+package basic;
+
+public class OOP_Lession {
+    public static void main(String[] args) {
+        /*
+        OOP
+        Object Oriented Programming Language
+
+        Object
+        Class
+        Inheritance
+        Polymorphism
+        Abstraction
+        Encapsulation
+         */
+    }
+}
